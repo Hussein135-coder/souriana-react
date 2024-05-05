a34fe016640ba2fd5574cba4958225654273fa6a
Hussein <husseinsym3@gmail.com>
2024-05-05 22:07:46 +0300

    Remove 404.html And Add Souriana Images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import transition from "../transition";
+import syredu from "../assets/images/syredu.jpeg";
 
 const Home = () => {
-  return <div className="text-white text-center text-4xl mt-52">Home</div>;
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-64px)]">
+      <img src={syredu} className="sm:w-[25%] w-[50%] rounded-[50%]" />
+    </div>
+  );
 };
 
 export default transition(Home);
